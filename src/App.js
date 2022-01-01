8e7769a1632efe8b6d7c5445a16c608c5239f57a
Chetna <chetnap52@gmail.com>
2022-01-01 07:59:24 +0530

    Used destructuring and default prop values

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import MyComponent from "./MyComponent.jsx";
 
 function App() {
+  // console.log(process.env);
   return (
     <div className="App">
-      <h1>Hello My Application</h1>
+      <h1>Hello My Application - {process.env.NODE_ENV}</h1>
       <MyComponent />
       <MyComponent user="Chetna" />
       <MyComponent user="Pandey" />
-      <MyComponent user="Pandey" age="22" />
+      <MyComponent user="Pandey" age="22" welcomeMsg="Hey there" />
       {/* @todo: Learn how to add text inside a component tags   */}
+      {/* @todo: Learn to add custom environment variable in cra */}
     </div>
   );
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,13 +1,14 @@
-function myComponent(props) {
-    var user = props.user || 'User';
-    console.log(props);
+function MyComponent({user:name = 'User', age, ...restProps}) {
+    // var user = props.user || 'User';
+    console.log(restProps);
     return <div>
         {/* @toNote: javascript expression under jsx code can be written under {} */}
-      <h3>Hello, {user} {props.age && <span>Your age is {props.age}</span>}</h3>
+      <h3>Hello, {name} {age && <span>Your age is {age}</span>}</h3>
+      <h4>{restProps.welcomeMsg}</h4>
     </div>
 }
 
-export default myComponent;
+export default MyComponent;
 
 // named export 
 // default export
